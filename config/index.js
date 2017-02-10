393bfb39e1cc26ef021c6c3bc88c41284d19424c
Khoa Nguyen <khoa@malonetuning.com>
2017-02-10 12:47:42 -0800

    clean up docs

--- config/index.js
+++ config/index.js
@@ -20,7 +20,7 @@ module.exports = {
     env: require('./dev.env'),
     port: 8080,
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/docs',
     proxyTable: {},
     // CSS Sourcemaps off by default because relative paths are "buggy"
     // with this option, according to the CSS-Loader README
--- docs/src/main.js
+++ docs/src/main.js
@@ -1,10 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-if (!process.env.NODE_ENV) {
-  process.env.NODE_ENV = 'development'
-}
-
 new Vue({
   el: '#app',
   render: h => h(App)
