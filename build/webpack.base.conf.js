df7969cc54953fbf91a7864cea67b28d45c1ea1c
Khoa Nguyen <khoa@malonetuning.com>
2017-02-15 10:23:50 -0800

    attempt fix #2

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -26,9 +26,7 @@ module.exports = {
     ],
     alias: {
       'vue$': 'vue/dist/vue.common.js',
-      'src': resolve('src'),
-      'assets': resolve('src/assets'),
-      'components': resolve('src/components')
+      'src': resolve('src')
     }
   },
   module: {
