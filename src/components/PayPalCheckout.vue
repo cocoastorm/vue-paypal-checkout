66a3aa25c12e3443a79e4be5c00634ae147e31a8
Khoa <khoa.tan.nguyen.96@gmail.com>
2017-09-21 12:50:51 -0700

    rewrite to airbnb style

--- src/components/PayPalCheckout.vue
+++ src/components/PayPalCheckout.vue
@@ -3,143 +3,144 @@
 </template>
 
 <script>
-  import shortid from 'shortid'
-  import paypal from 'paypal-checkout'
+  import shortid from 'shortid';
+  import paypal from 'paypal-checkout';
 
-  const validateButtonStyle = function (source) {
-    const copy = Object.assign({}, source)
+  function isValid(item, options) {
+    return options.some(v => (v === item));
+  }
+
+  function validateButtonStyle(source) {
+    const copy = Object.assign({}, source);
     const options = {
       size: ['tiny', 'small', 'medium', 'responsive'],
       color: ['gold', 'blue', 'silver'],
-      shape: ['pill', 'rect']
-    }
-
-    function isValid (item, options) {
-      return options.some((v) => {
-        return v === item
-      })
-    }
+      shape: ['pill', 'rect'],
+    };
 
     Object.keys(options).forEach((key) => {
-      const item = copy[key]
-      const valid = isValid(item, options[key])
+      const item = copy[key];
+      const valid = isValid(item, options[key]);
 
       if (!valid) {
-        console.warn(`style.${key} = \'${item}\' isn\'t a valid option`, options[key])
-        return false
+        console.warn(`style.${key} = '${item}' isn't a valid option`, options[key]);
+        return false;
       }
-    })
 
-    return true
+      return true;
+    });
+
+    return true;
   }
 
   export default {
     name: 'simple-paypal',
-    data: function () {
+    data() {
       return {
-        id: shortid.generate()
-      }
+        id: shortid.generate(),
+      };
     },
     props: {
       amount: {
         type: String,
-        required: true
+        required: true,
       },
       client: {
         type: Object,
-        required: true
+        required: true,
       },
       commit: {
         type: Boolean,
         required: false,
-        default: true
+        default: true,
       },
       currency: {
         type: String,
         required: false,
-        default: 'USD'
+        default: 'USD',
       },
       dev: {
         type: Boolean,
-        required: false
+        required: false,
       },
       invoiceNumber: {
         type: String,
-        required: false
+        required: false,
       },
       items: {
         type: Array,
-        required: false
+        required: false,
       },
       buttonStyle: {
         type: Object,
         required: false,
-        validator: validateButtonStyle
-      }
+        validator: validateButtonStyle,
+      },
     },
     computed: {
-      env: function () {
-        const env = process.env.NODE_ENV
+      env() {
+        const env = process.env.NODE_ENV;
 
         if (this.dev) {
-          return 'sandbox'
+          return 'sandbox';
         }
 
-        return (env !== 'production') ? 'sandbox' : 'production'
-      }
+        return (env !== 'production') ? 'sandbox' : 'production';
+      },
     },
     methods: {
-      item_list: function () {
-        let item_list = {
-          items: []
-        }
+      item_list() {
+        const itemList = {
+          items: [],
+        };
 
         this.items.forEach((item) => {
-          item_list.items.push(item)
-        })
+          itemList.items.push(item);
+        });
 
-        return item_list
+        return itemList;
       },
-      PayPalPayment: function () {
-        let transaction = {
+      PayPalPayment() {
+        const transaction = {
           amount: {
             total: this.amount,
-            currency: this.currency
-          }
-        }
+            currency: this.currency,
+          },
+        };
 
         if (this.invoiceNumber !== undefined) {
-          transaction.invoice_number = this.invoiceNumber
+          transaction.invoice_number = this.invoiceNumber;
         }
 
         if (this.items !== undefined) {
-          transaction.item_list = this.item_list()
+          transaction.item_list = this.item_list();
         }
 
         return paypal.rest.payment.create(this.env, this.client, {
-          transactions: [ transaction ]
-        })
+          transactions: [transaction],
+        });
       },
-      onAuthorize: function (data, actions) {
-        const vue = this
-        vue.$emit('paypal-paymentAuthorized', data)
+      onAuthorize(data, actions) {
+        const vue = this;
+        vue.$emit('paypal-paymentAuthorized', data);
         return actions.payment.execute().then((response) => {
-          vue.$emit('paypal-paymentCompleted', response)
-        })
+          vue.$emit('paypal-paymentCompleted', response);
+        });
       },
-      onCancel: function (data) {
-        const vue = this
-        vue.$emit('paypal-paymentCancelled', data)
-      }
+      onCancel(data) {
+        const vue = this;
+        vue.$emit('paypal-paymentCancelled', data);
+      },
     },
-    mounted: function () {
-      const vue = this
-      
+    mounted() {
+      const vue = this;
+
       const buttonObject = {
         // Pass in env
         env: vue.env,
 
-        // Pass in the client ids to use to create your transaction on sandbox and production environments
+        // Pass in the client ids to use to create your transaction
+        // on sandbox and production environments
         client: vue.client,
 
         // Pass the payment details for your transaction
@@ -153,15 +154,15 @@
         onAuthorize: vue.onAuthorize,
 
         // Pass a function to be called when the customer cancels the payment
-        onCancel: vue.onCancel
-      }
+        onCancel: vue.onCancel,
+      };
 
       // validate style prop
       if (vue.buttonStyle !== undefined) {
-        buttonObject.style = vue.buttonStyle
+        buttonObject.style = vue.buttonStyle;
       }
 
-      paypal.Button.render(buttonObject, vue.id)
-    }
-  }
+      paypal.Button.render(buttonObject, vue.id);
+    },
+  };
 </script>
