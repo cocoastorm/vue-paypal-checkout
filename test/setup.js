8143ef43f17f4710cae0a11d21ad10d7d3656f80
Khoa <khoa.tan.nguyen.96@gmail.com>
2017-12-15 15:25:56 -0800

    remove chai

--- test/setup.js
+++ test/setup.js
@@ -1,8 +1,3 @@
-import { expect } from 'chai';
-
-global.jestExpect = global.expect;
-global.expect = expect;
-
 // mock window for `paypal-checkout`
 Object.defineProperty(window.location, 'host', {
   writeable: true,
