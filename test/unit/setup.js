a3407ff9fbf213b358d0bf4dd2839d8656f30158
Khoa <khoa.tan.nguyen.96@gmail.com>
2017-12-18 15:39:33 -0800

    clean up jest configuration

--- test/unit/setup.js
+++ test/unit/setup.js
@@ -1,3 +1,7 @@
+import Vue from 'vue';
+
+Vue.config.productionTip = false;
+
 // mock window for `paypal-checkout`
 Object.defineProperty(window.location, 'host', {
   writeable: true,
---
+++ test/unit/specs/util/propHookManager.spec.js
@@ -0,0 +1,21 @@
+import PropHook from '@/util/propHook';
+import PropHookManager from '@/util/propHookManager';
+
+const getPropHooks = (type, num) => {
+  const hooks = [];
+  for (let i = 0; i < num; i += 1) {
+    hooks[i] = new PropHook(`some-name-${num}`, `some-paypal-name-${num}`, type);
+  }
+  return hooks;
+};
+
+const getHooks = () => {
+  return {
+    transactions: getPropHooks('transaction', 10),
+    buttons: getPropHooks('button', 10),
+  };
+};
+
+describe('PropHookManager', () => {
+  const hooks = getHooks();
+});
