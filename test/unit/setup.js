e8791a721c05b85e07b5a914514a8dfcb01122ef
Khoa <khoa.tan.nguyen.96@gmail.com>
2018-01-22 15:03:41 -0800

    Revert "don't need to mock window.location.host"
    This reverts commit cdc2ae0bc95d519c47993111cfcd0da53d2a870e.

--- test/unit/setup.js
+++ test/unit/setup.js
@@ -1,3 +1,9 @@
 import Vue from 'vue';
 
 Vue.config.productionTip = false;
+
+// mock window for `paypal-checkout`
+Object.defineProperty(window.location, 'host', {
+  writeable: true,
+  value: 'localhost',
+});
