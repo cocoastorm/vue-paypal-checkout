ced6d4dd7f391e0ffb77c74a0d57101aedf21148
Khoa <khoa.tan.nguyen.96@gmail.com>
2018-01-22 16:45:31 -0800

    don't include stylesheets

--- test/unit/jest.conf.js
+++ test/unit/jest.conf.js
@@ -13,6 +13,7 @@ module.exports = {
   transform: {
     '^.+\\.js$': '<rootDir>/node_modules/babel-jest',
     '.*\\.(vue)$': '<rootDir>/node_modules/vue-jest',
+    '\\.(css|less|scss)$': '<rootDir>/test/__mocks__/styleMock.js',
   },
   snapshotSerializers: ['<rootDir>/node_modules/jest-serializer-vue'],
   setupFiles: ['<rootDir>/test/unit/setup'],
