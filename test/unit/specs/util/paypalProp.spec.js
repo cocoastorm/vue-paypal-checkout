f2b186ffe3e6e89d8a2729dbad5a58161b3664ff
Khoa <khoa.tan.nguyen.96@gmail.com>
2018-02-13 11:10:34 -0800

    test minimal constructor with just 'name'

--- test/unit/specs/util/paypalProp.spec.js
+++ test/unit/specs/util/paypalProp.spec.js
@@ -1,25 +1,37 @@
 import PayPalProp from '@/util/paypalProp';
 
 describe('paypalProp.js', () => {
-  it('construct with options object', () => {
-    const paypal = new PayPalProp({
-      name: 'some-name',
-      paypalName: 'somePaypal',
-      injectionType: 'someExperience',
+  describe('new paypalProp()', () => {
+    it('construct with most options specified', () => {
+      const paypal = new PayPalProp({
+        name: 'some-name',
+        paypalName: 'somePaypal',
+        injection: 'someExperience',
+      });
+
+      expect(paypal).toEqual(expect.objectContaining({
+        name: 'some-name',
+        propName: 'somePaypal',
+        injection: 'someExperience',
+      }));
     });
 
-    expect(paypal).toEqual(expect.objectContaining({
-      name: 'some-name',
-      propName: 'somePaypal',
-      injection: 'someExperience',
-    }));
+    it('construct with just name specified', () => {
+      const paypal = new PayPalProp({ name: 'some-name' });
+
+      expect(paypal).toEqual(expect.objectContaining({
+        name: 'some-name',
+        propName: 'some-name',
+        injection: 'button',
+      }));
+    });
   });
 
   it('getVmProp()', () => {
     const paypal = new PayPalProp({
       name: 'some-name',
       paypalName: 'somePaypal',
-      injectionType: 'someExperience',
+      injection: 'someExperience',
     });
 
     const prop = paypal.getVmProp();
@@ -34,7 +46,7 @@ describe('paypalProp.js', () => {
     const paypal = new PayPalProp({
       name: 'some-name',
       paypalName: 'somePaypal',
-      injectionType: 'someExperience',
+      injection: 'someExperience',
     });
 
     const o = {
