cbb5bbcba737b9394d327c90d09c498eb616a8d0
Khoa <khoa.tan.nguyen.96@gmail.com>
2018-03-09 10:45:49 -0800

    remove mapCoverage, no longer needed (deprecated)

--- test/unit/jest.conf.js
+++ test/unit/jest.conf.js
@@ -17,7 +17,6 @@ module.exports = {
   },
   snapshotSerializers: ['<rootDir>/node_modules/jest-serializer-vue'],
   setupFiles: ['<rootDir>/test/unit/setup'],
-  mapCoverage: true,
   coverageDirectory: '<rootDir>/test/unit/coverage',
   collectCoverageFrom: [
     'src/**/*.{js,vue}',
