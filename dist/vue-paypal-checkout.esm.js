7d3c798425bb9847ddbd0c9ebeb1d7c9a00411bf
Khoa <khoa.tan.nguyen.96@gmail.com>
2018-06-21 16:18:46 -0700

    build for 3.1.3

--- dist/vue-paypal-checkout.esm.js
+++ dist/vue-paypal-checkout.esm.js
@@ -1,6 +1,7 @@
 import _slicedToArray from 'babel-runtime/helpers/slicedToArray';
 import _Object$assign from 'babel-runtime/core-js/object/assign';
 import paypal from 'paypal-checkout';
+import 'babel-runtime/core-js/json/stringify';
 import _Object$keys from 'babel-runtime/core-js/object/keys';
 
 var requiredProps = [['amount'], ['currency', 'USD']];
@@ -15,9 +16,23 @@ var specificProps = [{
   validator: function validator(value) {
     return ['sandbox', 'production'].indexOf(value) !== -1;
   }
-}, { name: 'client', type: Object, required: true }, { name: 'details', type: Object, required: false, default: {} },
-// eslint-disable-next-line
-{ name: 'commit', type: Boolean, required: false, default: true }];
+}, {
+  name: 'client',
+  type: Object,
+  required: true
+}, {
+  name: 'details',
+  type: Object,
+  required: false,
+  default: function _default() {
+    return {};
+  }
+}, {
+  name: 'commit',
+  type: Boolean,
+  required: false,
+  default: true
+}];
 
 function defaultProps () {
   var props = {};
@@ -197,9 +212,7 @@ var additionalProps = {
 
 var assignTo = assignToPropertyObject(additionalProps);
 
-var PayPalCheckout = { render: function render() {
-    var _vm = this;var _h = _vm.$createElement;var _c = _vm._self._c || _h;return _c('div', { staticClass: "paypal-button", attrs: { "id": _vm.id } });
-  }, staticRenderFns: [],
+var script = {
   props: _Object$assign(defaultProps(), additionalProps.vmProps()),
   methods: {
     payment: function payment() {
@@ -222,9 +235,14 @@ var PayPalCheckout = { render: function render() {
     onAuthorize: function onAuthorize(data, actions) {
       var vue = this;
       vue.$emit('payment-authorized', data);
-      return actions.payment.execute().then(function (response) {
-        vue.$emit('payment-completed', response);
-      });
+
+      if (this.commit) {
+        return actions.payment.execute().then(function (response) {
+          vue.$emit('payment-completed', response);
+        });
+      }
+
+      return true;
     },
     onCancel: function onCancel(data) {
       var vue = this;
@@ -258,6 +276,103 @@ var PayPalCheckout = { render: function render() {
     paypal.Button.render(button, vue.$el);
   }
 };
+
+var __vue_script__ = script;
+
+/* template */
+var __vue_render__ = function __vue_render__() {
+  var _vm = this;
+  var _h = _vm.$createElement;
+  var _c = _vm._self._c || _h;
+  return _c("div", { staticClass: "paypal-button", attrs: { id: _vm.id } });
+};
+var __vue_staticRenderFns__ = [];
+__vue_render__._withStripped = true;
+
+var __vue_template__ = typeof __vue_render__ !== 'undefined' ? { render: __vue_render__, staticRenderFns: __vue_staticRenderFns__ } : {};
+/* style */
+var __vue_inject_styles__ = undefined;
+/* scoped */
+var __vue_scope_id__ = undefined;
+/* module identifier */
+var __vue_module_identifier__ = undefined;
+/* functional template */
+var __vue_is_functional_template__ = false;
+/* component normalizer */
+function __vue_normalize__(template, style, script$$1, scope, functional, moduleIdentifier, createInjector, createInjectorSSR) {
+  var component = (typeof script$$1 === 'function' ? script$$1.options : script$$1) || {};
+
+  {
+    component.__file = "/home/khoa/Code/paypal/src/components/PayPalCheckout.vue";
+  }
+
+  if (!component.render) {
+    component.render = template.render;
+    component.staticRenderFns = template.staticRenderFns;
+    component._compiled = true;
+
+    if (functional) component.functional = true;
+  }
+
+  component._scopeId = scope;
+
+  return component;
+}
+/* style inject */
+function __vue_create_injector__() {
+  var head = document.head || document.getElementsByTagName('head')[0];
+  var styles = __vue_create_injector__.styles || (__vue_create_injector__.styles = {});
+  var isOldIE = typeof navigator !== 'undefined' && /msie [6-9]\\b/.test(navigator.userAgent.toLowerCase());
+
+  return function addStyle(id, css) {
+    if (document.querySelector('style[data-vue-ssr-id~="' + id + '"]')) return; // SSR styles are present.
+
+    var group = isOldIE ? css.media || 'default' : id;
+    var style = styles[group] || (styles[group] = { ids: [], parts: [], element: undefined });
+
+    if (!style.ids.includes(id)) {
+      var code = css.source;
+      var index = style.ids.length;
+
+      style.ids.push(id);
+
+      if (isOldIE) {
+        style.element = style.element || document.querySelector('style[data-group=' + group + ']');
+      }
+
+      if (!style.element) {
+        var el = style.element = document.createElement('style');
+        el.type = 'text/css';
+
+        if (css.media) el.setAttribute('media', css.media);
+        if (isOldIE) {
+          el.setAttribute('data-group', group);
+          el.setAttribute('data-next-index', '0');
+        }
+
+        head.appendChild(el);
+      }
+
+      if (isOldIE) {
+        index = parseInt(style.element.getAttribute('data-next-index'));
+        style.element.setAttribute('data-next-index', index + 1);
+      }
+
+      if (style.element.styleSheet) {
+        style.parts.push(code);
+        style.element.styleSheet.cssText = style.parts.filter(Boolean).join('\n');
+      } else {
+        var textNode = document.createTextNode(code);
+        var nodes = style.element.childNodes;
+        if (nodes[index]) style.element.removeChild(nodes[index]);
+        if (nodes.length) style.element.insertBefore(textNode, nodes[index]);else style.element.appendChild(textNode);
+      }
+    }
+  };
+}
+/* style inject SSR */
+
+var PayPalCheckout = __vue_normalize__(__vue_template__, __vue_inject_styles__, typeof __vue_script__ === 'undefined' ? {} : __vue_script__, __vue_scope_id__, __vue_is_functional_template__, __vue_module_identifier__, typeof __vue_create_injector__ !== 'undefined' ? __vue_create_injector__ : function () {}, typeof __vue_create_injector_ssr__ !== 'undefined' ? __vue_create_injector_ssr__ : function () {});
 
 var components = {
   'paypal-checkout': PayPalCheckout
