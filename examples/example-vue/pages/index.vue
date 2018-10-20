685ee6e038dd8c24f47a9419d08cb0a0b95d1298
suryaadtmaja <suryaadtmaja13@gmail.com>
2018-10-20 16:32:22 +0700

    adding nuxt js example and updating the documentation

---
+++ examples/example-vue/pages/index.vue
@@ -0,0 +1,67 @@
+<template>
+  <section class="container">
+    <div>
+      <p>Pay with USD $10.00</p>
+      <no-ssr>
+        <paypal-checkout
+          amount="10.00"
+          currency="USD"
+          env="sandbox"
+          :client="paypal"
+          :invoice-number="'201705051001'">
+        </paypal-checkout>
+      </no-ssr>
+    </div>
+  </section>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  },
+  data() {
+    return {
+      paypal: {
+        sandbox: 'Ad1voWYq3VL8J4jy6zWARKwz4tjbuDl_TFBa3WQqy_DwAFWd7hkU4i99jijGqaoqU3E-ODqXDayVnOdl',
+        production: '',
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
