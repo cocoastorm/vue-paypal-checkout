618e9a5e1f446a3c68493fead5d1b6a3dcea5cfe
Khoa <khoa.tan.nguyen.96@gmail.com>
2018-11-27 10:33:20 -0800

    fix(lint): small hiccup in test/unit/setup.js

--- test/unit/setup.js
+++ test/unit/setup.js
@@ -3,9 +3,8 @@ import Vue from 'vue';
 Vue.config.productionTip = false;
 
 // polyfill matchMedia
-window.matchMedia = window.matchMedia ||
-  (() => ({
-    matches: false,
-    addListener: () => {},
-    removeListener: () => {},
-  }));
+window.matchMedia = window.matchMedia || (() => ({
+  matches: false,
+  addListener: () => {},
+  removeListener: () => {},
+}));
