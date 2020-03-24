1920bf3f64c9ca3a1fea14cba53275f930806a5b
onkar <onkarskore@gmail.com>
2020-03-24 12:54:42 +0530

    dist updated

--- dist/vue-paypal-checkout.esm.js
+++ dist/vue-paypal-checkout.esm.js
@@ -76,6 +76,8 @@ function defaultProps () {
     }
   });
 
+  props['send-paypal-actions'] = { type: Function, required: false, default: undefined };
+
   return props;
 }
 
@@ -221,6 +223,9 @@ var assignTo = assignToPropertyObject(additionalProps);
 var script = {
   props: _Object$assign(defaultProps(), additionalProps.vmProps()),
   methods: {
+    validate: function validate(actions) {
+      this.$emit('send-paypal-actions', actions);
+    },
     payment: function payment() {
       var vue = this;
 
@@ -282,12 +287,100 @@ var script = {
       onAuthorize: vue.onAuthorize,
 
       // Pass a function to be called when the customer cancels the payment
-      onCancel: vue.onCancel
+      onCancel: vue.onCancel,
+
+      // Pass a function to be called when the page load
+      validate: vue.validate
     }, assignTo(vue, propTypes.BUTTON));
 
     paypal.Button.render(button, vue.$el);
   }
 };
+
+function normalizeComponent(template, style, script, scopeId, isFunctionalTemplate, moduleIdentifier
+/* server only */
+, shadowMode, createInjector, createInjectorSSR, createInjectorShadow) {
+  if (typeof shadowMode !== 'boolean') {
+    createInjectorSSR = createInjector;
+    createInjector = shadowMode;
+    shadowMode = false;
+  } // Vue.extend constructor export interop.
+
+
+  var options = typeof script === 'function' ? script.options : script; // render functions
+
+  if (template && template.render) {
+    options.render = template.render;
+    options.staticRenderFns = template.staticRenderFns;
+    options._compiled = true; // functional template
+
+    if (isFunctionalTemplate) {
+      options.functional = true;
+    }
+  } // scopedId
+
+
+  if (scopeId) {
+    options._scopeId = scopeId;
+  }
+
+  var hook;
+
+  if (moduleIdentifier) {
+    // server build
+    hook = function hook(context) {
+      // 2.3 injection
+      context = context || // cached call
+      this.$vnode && this.$vnode.ssrContext || // stateful
+      this.parent && this.parent.$vnode && this.parent.$vnode.ssrContext; // functional
+      // 2.2 with runInNewContext: true
+
+      if (!context && typeof __VUE_SSR_CONTEXT__ !== 'undefined') {
+        context = __VUE_SSR_CONTEXT__;
+      } // inject component styles
+
+
+      if (style) {
+        style.call(this, createInjectorSSR(context));
+      } // register component module identifier for async chunk inference
+
+
+      if (context && context._registeredComponents) {
+        context._registeredComponents.add(moduleIdentifier);
+      }
+    }; // used by ssr in case component is cached and beforeCreate
+    // never gets called
+
+
+    options._ssrRegister = hook;
+  } else if (style) {
+    hook = shadowMode ? function () {
+      style.call(this, createInjectorShadow(this.$root.$options.shadowRoot));
+    } : function (context) {
+      style.call(this, createInjector(context));
+    };
+  }
+
+  if (hook) {
+    if (options.functional) {
+      // register for functional component in vue file
+      var originalRender = options.render;
+
+      options.render = function renderWithStyleInjection(h, context) {
+        hook.call(context);
+        return originalRender(h, context);
+      };
+    } else {
+      // inject component registration as beforeCreate hook
+      var existing = options.beforeCreate;
+      options.beforeCreate = existing ? [].concat(existing, hook) : [hook];
+    }
+  }
+
+  return script;
+}
+
+var normalizeComponent_1 = normalizeComponent;
 
 /* script */
 var __vue_script__ = script;
@@ -310,30 +403,11 @@ var __vue_scope_id__ = undefined;
 var __vue_module_identifier__ = undefined;
 /* functional template */
 var __vue_is_functional_template__ = false;
-/* component normalizer */
-function __vue_normalize__(template, style, script$$1, scope, functional, moduleIdentifier, createInjector, createInjectorSSR) {
-  var component = (typeof script$$1 === 'function' ? script$$1.options : script$$1) || {};
-
-  // For security concerns, we use only base name in production mode.
-  component.__file = "/home/khoa/src/github.com/khoanguyen96/paypal/src/components/PayPalCheckout.vue";
-
-  if (!component.render) {
-    component.render = template.render;
-    component.staticRenderFns = template.staticRenderFns;
-    component._compiled = true;
-
-    if (functional) component.functional = true;
-  }
-
-  component._scopeId = scope;
-
-  return component;
-}
 /* style inject */
 
 /* style inject SSR */
 
-var PayPalCheckout = __vue_normalize__({ render: __vue_render__, staticRenderFns: __vue_staticRenderFns__ }, __vue_inject_styles__, __vue_script__, __vue_scope_id__, __vue_is_functional_template__, __vue_module_identifier__, undefined, undefined);
+var PayPalCheckout = normalizeComponent_1({ render: __vue_render__, staticRenderFns: __vue_staticRenderFns__ }, __vue_inject_styles__, __vue_script__, __vue_scope_id__, __vue_is_functional_template__, __vue_module_identifier__, undefined, undefined);
 
 var components = {
   'paypal-checkout': PayPalCheckout
